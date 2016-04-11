64c22595965e129f238923f12fbae827061bb11d
msut <marr994@o2.pl>
2016-04-11 18:10:06 +0200

    add support for large screens

--- app/src/main/java/com/astroweather/MainActivity.java
+++ app/src/main/java/com/astroweather/MainActivity.java
@@ -1,22 +1,36 @@
 package com.astroweather;
 
+
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import com.astroweather.fragments.MoonFragment;
+import com.astroweather.fragments.SunFragment;
 import com.astuetz.PagerSlidingTabStrip;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int NUMBER_OF_TABS = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
-        pager.setAdapter(new FragmentAdapter(getSupportFragmentManager(), 2));
 
-        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
-        tabs.setViewPager(pager);
+        ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
+        if (pager != null) {
+            pager.setAdapter(new FragmentAdapter(getSupportFragmentManager(), NUMBER_OF_TABS));
+            PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+            tabs.setViewPager(pager);
+        } else {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.sun_fragment, new SunFragment());
+            fragmentTransaction.add(R.id.moon_fragment, new MoonFragment());
+            fragmentTransaction.commit();
+        }
+
+
     }
 }
