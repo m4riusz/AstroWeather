7e6aa24c4799b86431c463492806cbc830b19dc7
msut <marr994@o2.pl>
2016-04-11 18:33:53 +0200

    add menu items

--- app/src/main/java/com/astroweather/MainActivity.java
+++ app/src/main/java/com/astroweather/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.astroweather.fragments.MoonFragment;
 import com.astroweather.fragments.SunFragment;
@@ -13,6 +16,8 @@ import com.astuetz.PagerSlidingTabStrip;
 public class MainActivity extends AppCompatActivity {
 
     public static final int NUMBER_OF_TABS = 2;
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +35,24 @@ public class MainActivity extends AppCompatActivity {
             fragmentTransaction.add(R.id.moon_fragment, new MoonFragment());
             fragmentTransaction.commit();
         }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_localization:
+                Log.i("WTF", "localization selected: ");
+                return true;
+            case R.id.menu_delay:
+                Log.i("WTF", "delay selected");
+                return true;
+        }
+        return false;
     }
 }
