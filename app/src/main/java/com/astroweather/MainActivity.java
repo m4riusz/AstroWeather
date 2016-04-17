2da577ac85404ccf0f683531b08d1e912656fa13
msut <marr994@o2.pl>
2016-04-17 18:31:26 +0200

    add time view to layout

--- app/src/main/java/com/astroweather/MainActivity.java
+++ app/src/main/java/com/astroweather/MainActivity.java
@@ -24,7 +24,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            initTime();
+        }
         determineScreenType();
+    }
+
+    private void initTime() {
+        //TODO time
     }
 
     private void determineScreenType() {
