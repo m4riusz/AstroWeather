ee8dd24434322687470979279051c3a8d079d2b3
msut <marr994@o2.pl>
2016-05-20 17:57:30 +0200

    add localization activity

---
+++ app/src/main/java/com/astroweather/LocalizationActivity.java
@@ -0,0 +1,10 @@
+package com.astroweather;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mariusz on 20.05.16.
+ */
+public class LocalizationActivity extends AppCompatActivity {
+
+}
