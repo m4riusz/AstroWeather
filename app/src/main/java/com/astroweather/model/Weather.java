a7e6459be03429234f66ce77f13e327de72ffd09
msut <marr994@o2.pl>
2016-05-24 18:51:12 +0200

    add weather model

---
+++ app/src/main/java/com/astroweather/model/Weather.java
@@ -0,0 +1,72 @@
+package com.astroweather.model;
+
+import java.util.Date;
+
+/**
+ * Created by mariusz on 24.05.16.
+ */
+public class Weather {
+    private Date date;
+    private int temperature;
+    private float humidity;
+    private float pressure;
+    private float windSpeed;
+    private float windDirection;
+
+    public Weather(Date date, int temperature, float humidity, float pressure, float windSpeed, float windDirection) {
+        this.date = date;
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        this.windSpeed = windSpeed;
+        this.windDirection = windDirection;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(float humidity) {
+        this.humidity = humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(float pressure) {
+        this.pressure = pressure;
+    }
+
+    public float getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(float windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public float getWindDirection() {
+        return windDirection;
+    }
+
+    public void setWindDirection(float windDirection) {
+        this.windDirection = windDirection;
+    }
+}
