0874da0d4bc4f9312c228193e6fde984aee76042
msut <marr994@o2.pl>
2016-05-24 19:07:06 +0200

    add weather list to localization

--- app/src/main/java/com/astroweather/model/Localization.java
+++ app/src/main/java/com/astroweather/model/Localization.java
@@ -3,6 +3,8 @@ package com.astroweather.model;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.util.List;
+
 /**
  * Created by mariusz on 20.05.16.
  */
@@ -12,17 +14,20 @@ public class Localization implements Parcelable {
     private String name;
     private double longitude;
     private double latitude;
+    private List<Weather> weathers;
 
-    public Localization(String name, double longitude, double latitude) {
+    public Localization(String name, double longitude, double latitude, List<Weather> weathers) {
         this.name = name;
         this.longitude = longitude;
         this.latitude = latitude;
+        this.weathers = weathers;
     }
 
     protected Localization(Parcel in) {
         name = in.readString();
         longitude = in.readDouble();
         latitude = in.readDouble();
+        in.readList(weathers, null);
     }
 
     public String getName() {
@@ -59,6 +64,7 @@ public class Localization implements Parcelable {
         parcel.writeString(name);
         parcel.writeDouble(longitude);
         parcel.writeDouble(latitude);
+        parcel.writeList(weathers);
     }
 
     public static final Creator<Localization> CREATOR = new Creator<Localization>() {
--- app/src/main/java/com/astroweather/model/Weather.java
+++ app/src/main/java/com/astroweather/model/Weather.java
@@ -1,25 +1,40 @@
 package com.astroweather.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Date;
 
 /**
  * Created by mariusz on 24.05.16.
  */
-public class Weather {
+public class Weather implements Parcelable {
     private Date date;
     private int temperature;
     private float humidity;
     private float pressure;
     private float windSpeed;
     private float windDirection;
+    private float clouds;
 
-    public Weather(Date date, int temperature, float humidity, float pressure, float windSpeed, float windDirection) {
+    public Weather(Date date, int temperature, float humidity, float pressure, float windSpeed, float windDirection, float clouds) {
         this.date = date;
         this.temperature = temperature;
         this.humidity = humidity;
         this.pressure = pressure;
         this.windSpeed = windSpeed;
         this.windDirection = windDirection;
+        this.clouds = clouds;
+    }
+
+    protected Weather(Parcel in) {
+        date = (Date) in.readSerializable();
+        temperature = in.readInt();
+        humidity = in.readFloat();
+        pressure = in.readFloat();
+        windSpeed = in.readFloat();
+        windDirection = in.readFloat();
+        clouds = in.readFloat();
     }
 
     public Date getDate() {
@@ -68,5 +83,41 @@ public class Weather {
 
     public void setWindDirection(float windDirection) {
         this.windDirection = windDirection;
+    }
+
+    public float getClouds() {
+        return clouds;
+    }
+
+    public void setClouds(float clouds) {
+        this.clouds = clouds;
+    }
+
+    public static final Creator<Weather> CREATOR = new Creator<Weather>() {
+        @Override
+        public Weather createFromParcel(Parcel in) {
+            return new Weather(in);
+        }
+
+        @Override
+        public Weather[] newArray(int size) {
+            return new Weather[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeSerializable(date);
+        parcel.writeInt(temperature);
+        parcel.writeFloat(humidity);
+        parcel.writeFloat(pressure);
+        parcel.writeFloat(windSpeed);
+        parcel.writeFloat(windDirection);
+        parcel.writeFloat(clouds);
     }
 }
