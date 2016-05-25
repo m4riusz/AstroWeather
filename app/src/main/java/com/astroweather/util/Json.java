ffcf0515ae846bcc84229e3057cc918d3be7025b
msut <marr994@o2.pl>
2016-05-25 20:39:31 +0200

    create json util

---
+++ app/src/main/java/com/astroweather/util/Json.java
@@ -0,0 +1,16 @@
+package com.astroweather.util;
+
+public class Json {
+    public static final String WEATHER_LIST = "list";
+    public static final String JSON_DATE_FORMAT = "yyyy-MM-dd hh:mm:ss";
+    public static final String DATE_STRING = "dt_txt";
+    public static final String BASIC = "main";
+    public static final String TEMPERATURE = "temp";
+    public static final String HUMIDITY = "humidity";
+    public static final String PRESSURE = "pressure";
+    public static final String SPEED = "speed";
+    public static final String DIRECTION = "deg";
+    public static final String WIND = "wind";
+    public static final String CLOUDS = "clouds";
+    public static final String PERCENTAGE = "all";
+}
